35a7b7bfb0ff94bffe1b3fed2a00e036458c0fe1
Abderrahmane Laajili <abderrahmanelaajili0@gmail.com>
2023-09-30 17:31:01 +0100

    gradient descent

--- LogisticRegression/LogisticRegression.py
+++ LogisticRegression/LogisticRegression.py
@@ -6,23 +6,22 @@ class LogisticRegression:
     def __init__(self, learning_rate=0.05, epochs=500) -> None:
         self.learning_rate = learning_rate
         self.epochs = epochs
-    
-    
+
     def __sigmoid(self, z: float) -> float:
         return 1 / (1 + np.exp(-z))
 
-    def __gradient_descent(self, learning_rate, epochs):
+    def __gradient_descent(self, theta, X, y, learning_rate, epochs):
         
         for _ in range(epochs):
-            
 
     def fit(self, X: np.ndarray, y: np.ndarray) -> None:
         labels = np.unique(y)
         num_labels = y.shape[0]
         m, n = X.shape
-        theta = np.zeros((num_labels, n))
+        theta_matrix = np.zeros((num_labels, n))
 
         for label in labels:
             yOneVsAll = np.where((y == label), 1, 0)
+            theta = np.zeros(n)
             print(label, yOneVsAll)
 
